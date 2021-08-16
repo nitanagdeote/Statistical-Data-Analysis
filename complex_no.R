8cbf50e1105fe976d76126dc357c52d095176daf
Nita <nitanagdeote1@gmail.com>
2021-08-16 18:21:44 -0400

    Create complex_no.R

---
+++ complex_no.R
@@ -0,0 +1,16 @@
+#exponential function
+
+n=100
+x<-runif(n,min=-1,max=1)
+#x
+f<-exp(x)
+plot(x,f)
+################################3
+number=200
+z<-complex(real = rnorm(number),imaginary = rnorm(number))
+ex<-exp(z)
+head(z)
+#plot(z)
+plot(ex)
+head(ex)
+plot(z,ex)
