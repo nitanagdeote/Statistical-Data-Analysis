7ed66a173c3d63508774535128d89e829e81ab02
Nita <nitanagdeote1@gmail.com>
2021-09-02 23:07:50 -0400

    Create normal_dist.R

---
+++ normal_dist.R
@@ -0,0 +1,13 @@
+# normal distribution
+#rnorm() random normal distribution
+#rnorm has mean=0, sd=1 is default
+
+# pnorm probability function
+ 
+rnorm(10)
+rnorm(10,mean=100,sd=1)
+pnorm(0)
+pnorm(-1.96)
+pnorm(-1.64)
+qnorm(0.05)
+dnorm(0)
