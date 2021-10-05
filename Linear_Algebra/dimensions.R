cc10b6cdeb1bb4ec467c8a10d69fb256bf75a7a0
Nita <nitanagdeote1@gmail.com>
2021-10-05 12:13:50 -0400

    Create dimensions.R

---
+++ Linear_Algebra/dimensions.R
@@ -0,0 +1,16 @@
+# dimensions 
+#A point has a dimension of zero. 
+#In math, a point is assumed to be a dot with no size (no length or width).
+#A line or line segment has a dimension of one. 
+# 0 dimension
+#############################################
+y <- 10
+plot(y,type='p',col='blue',lwd=5)
+
+#############################################
+#A line or line segment has a dimension of one. It has a length. 
+# 1 dimension R^1 = R
+x <- c(1,2,4,5,6)
+plot(x,type='l',col='red',lwd=5)
+
+##############################################
