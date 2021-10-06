8ddc58d27acb733ee996bfcaa1740688618759ff
Nita <nitanagdeote1@gmail.com>
2021-10-06 17:26:58 -0400

    Create vector_plot.R

---
+++ Linear_Algebra/vector_plot.R
@@ -0,0 +1,8 @@
+#plot vectors with arrow
+plot(c(0,1),c(0,1),type='l')
+arrows(0,0,1,1)
+plot(c(0,3),c(0,2),type='l')
+arrows(0,0,3,2)
+plot(c(0,4),c(0,6),type='l',xlim = c(0,10),ylim = c(0,10))
+arrows(0,0,4,6)
+
