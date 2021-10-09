04ed7668e4c54ba26f43e7df8ce56b7407015774
Nita <nitanagdeote1@gmail.com>
2021-10-09 17:01:31 -0400

    Create par_mfrow.R

---
+++ Files/par_mfrow.R
@@ -0,0 +1,7 @@
+#using par(mfrow)
+attach(mtcars)
+par(mfrow=c(2,2))
+plot(wt,mpg, main="Scatterplot of wt vs. mpg")
+plot(wt,disp, main="Scatterplot of wt vs disp")
+hist(wt, main="Histogram of wt")
+boxplot(wt, main="Boxplot of wt")
