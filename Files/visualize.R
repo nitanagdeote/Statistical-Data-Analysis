3078234fb361b298498d422f07357bd4635b2ebc
Nita <nitanagdeote1@gmail.com>
2021-10-20 23:20:14 -0400

    Create visualize.R

---
+++ Files/visualize.R
@@ -0,0 +1,5 @@
+library(visualize)
+#visualize normal distribution
+visualize.norm(-2) # Area to the left of z value -2
+visualize.norm(-3) 
+visualize.norm(c(3,-3),section='tails') #default is lower means left
